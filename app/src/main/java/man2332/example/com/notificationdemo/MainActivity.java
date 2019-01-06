347bd4f2e402b82ed4313f3d754bbda60c60969b
john <poopman0202@gmail.com>
2019-01-06 18:48:33 -0500

    big picture style and media style notifications

--- app/src/main/java/man2332/example/com/notificationdemo/MainActivity.java
+++ app/src/main/java/man2332/example/com/notificationdemo/MainActivity.java
@@ -8,10 +8,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.MediaController;
 
 import static man2332.example.com.notificationdemo.App.CHANNEL_1_ID;
 import static man2332.example.com.notificationdemo.App.CHANNEL_2_ID;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTitle;
     private EditText editmessage;
 
+    private MediaSessionCompat mediaSessionCompat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +34,13 @@ public class MainActivity extends AppCompatActivity {
         editTitle = findViewById(R.id.edit_text_title);
         editmessage = findViewById(R.id.edit_text_message);
         notificationManagerCompat = NotificationManagerCompat.from(this);
-
-        /**
+        /**notificationManagerCompat
          * To use this class, call the static function from(Context) to get
          * a NotificationManagerCompat object, and then call one of its
          * methods to post or cancel notifications.
          */
+
+        mediaSessionCompat = new MediaSessionCompat(this, "tag");
 
     }
 
@@ -54,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0,
                 broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         //This bitmap is used to set a large icon in notification
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_one);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
 
         //Flag indicating that if the described PendingIntent already exists, then keep
         // it but replace its extra data with what is in this new Intent.
@@ -76,12 +81,30 @@ public class MainActivity extends AppCompatActivity {
                         .setSummaryText("Summary Text here"))
                 .build();
 
-        notificationManagerCompat.notify(1, notification);
+        Notification notification2 = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setSmallIcon(R.drawable.ic_one)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .setLargeIcon(bitmap)
+                .setStyle(new NotificationCompat.BigPictureStyle()//big pic style
+                        .bigPicture(bitmap)
+                        .bigLargeIcon(null))
+                .build();
+
+
+        notificationManagerCompat.notify(1, notification2);
     }
 
     public void sendOnChannel2(View view) {
         String title = editTitle.getText().toString();
         String message = editmessage.getText().toString();
+        //a bitmap that will be used for media style notification
+        Bitmap catPicture = BitmapFactory.decodeResource(getResources(), R.drawable.cat2);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setContentTitle(title)
@@ -101,7 +124,25 @@ public class MainActivity extends AppCompatActivity {
                         .addLine("Line7")
                 )
                 .build();
+        Notification notification2 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setSmallIcon(R.drawable.ic_two)
+                .setLargeIcon(catPicture)
+                .addAction(R.drawable.ic_dislike, "Dislike", null)
+                .addAction(R.drawable.ic_previous, "Previous", null)
+                .addAction(R.drawable.ic_pause, "Pause", null)
+                .addAction(R.drawable.ic_next, "Next", null)
+                .addAction(R.drawable.ic_like, "Like", null)
+                .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()//media style
+                        .setShowActionsInCompactView(1, 2, 3)
+                        .setMediaSession(mediaSessionCompat.getSessionToken()))//set a media session to a notification
+                .setSubText("Sub text here")
+                .build();
+        //TODO:set up the media session to work and make the buttons(like,dislike,etc) to work
 
-        notificationManagerCompat.notify(2, notification);
+        notificationManagerCompat.notify(2, notification2);
     }
 }
